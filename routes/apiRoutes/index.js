497f20d6346ee4983e2796c3bbb2fcbf8e1ee9c4
Brian <brian.lockerbie@gmail.com>
2022-06-08 01:04:30 -0700

    created lib w notes and added to routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,3 +5,20 @@ const { writeNote, dbUpdate, } = require("");
 const { notes } = require('');
 
 const { uuid } = require('uuidv4');
+
+router.get("/notes", (req, res) => {
+    let info = notes;
+    res.json(info);
+});
+
+router.post("/notes", (req, res) => {
+    let info = notes;
+    res.json(info);
+});
+
+router.delete("/notes", (req, res) => {
+    let info = notes;
+    res.json(info);
+});
+
+module.exports = router;
