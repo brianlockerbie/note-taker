3cb7f9a2474101499a919d89037ff365d1c4a74f
Brian <brian.lockerbie@gmail.com>
2022-06-07 23:42:40 -0700

    added app use's

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ const fs = require("fs");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+app.use(express.json());
